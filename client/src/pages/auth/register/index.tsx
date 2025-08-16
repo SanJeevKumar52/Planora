355a9be2e285b2859d3e2b207caf22152caaf3d6
sanjeevKalvig <sanjeev.kumar@kalvig.in>
2025-08-16 11:48:53 +0530

    Registration backend and fronted connected

--- client/src/pages/auth/register/index.tsx
+++ client/src/pages/auth/register/index.tsx
@@ -1,60 +1,76 @@
-
-import { Link } from "react-router-dom"
-import WelcomeContent from "../common/welcome-content"
-import { Button, Form, Input } from "antd"
+import { Link, useNavigate } from "react-router-dom";
+import WelcomeContent from "../common/welcome-content";
+import { Button, Form, Input, message } from "antd";
+import { registerUser } from "../../../api-services/users-service";
+import { useState } from "react";
 
 function RegisterPage() {
-    return (
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-            <div className="col-span-1 lg:flex hidden">
-                <WelcomeContent />
-            </div>
-            <div className="h-screen flex items-center justify-center">
-                <Form
-                    className="flex flex-col gap-5 w-96"
-                    layout="vertical"
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const onFinish = async (values: never) => {
+    try {
+      setLoading(true);
+      const response = await registerUser(values);
+      message.success(response.message || "Registration successful!");
+      navigate("/login");
+    } catch (error: any) {
+      message.error(error.response?.data.message || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                >
-                    <h1 className="text-2xl font-bold text-gray-600">
-                        Register your account
-                    </h1>
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="col-span-1 lg:flex hidden">
+        <WelcomeContent />
+      </div>
+      <div className="h-screen flex items-center justify-center">
+        <Form
+          className="flex flex-col gap-5 w-96"
+          layout="vertical"
+          onFinish={onFinish}
+        >
+          <h1 className="text-2xl font-bold text-gray-600">
+            Register your account
+          </h1>
 
-                    <Form.Item
-                        name="name"
-                        required
-                        label="Name"
-                        rules={[{ required: true }]}
-                    >
-                        <Input placeholder="Name" />
-                    </Form.Item>
+          <Form.Item
+            name="name"
+            required
+            label="Name"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="Name" />
+          </Form.Item>
 
-                    <Form.Item
-                        name="email"
-                        required
-                        label="Email"
-                        rules={[{ required: true }]}
-                    >
-                        <Input placeholder="Email" />
-                    </Form.Item>
+          <Form.Item
+            name="email"
+            required
+            label="Email"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
 
-                    <Form.Item
-                        name="password"
-                        required
-                        label="Password"
-                        rules={[{ required: true }]}
-                    >
-                        <Input.Password placeholder="Password" />
-                    </Form.Item>
+          <Form.Item
+            name="password"
+            required
+            label="Password"
+            rules={[{ required: true }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
 
-                    <Button type="primary" htmlType="submit" block >
-                        Register
-                    </Button>
+          <Button type="primary" htmlType="submit" block loading={loading}>
+            Register
+          </Button>
 
-                    <Link to="/login">Already have an account? Login</Link>
-                </Form>
-            </div>
-        </div>
-    )
+          <Link to="/login">Already have an account? Login</Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
