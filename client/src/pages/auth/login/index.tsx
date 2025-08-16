209e6c3b66e0f1ab81acfd53630e60fd40eb7f50
Sanjeev Kumar <sk3616593@gmail.com>
2025-08-16 12:23:05 +0530

    Login functionality done

--- client/src/pages/auth/login/index.tsx
+++ client/src/pages/auth/login/index.tsx
@@ -1,14 +1,28 @@
-
-import { Button, Form, Input} from "antd";
+import { Button, Form, Input } from "antd";
 import WelcomeContent from "../common/welcome-content";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import cookies from "js-cookie";
+import { loginUser } from "../../../api-services/users-service";
 
 function LoginPage() {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  /* const onFinish = async (values) => {
-  }; */
+  const onFinish = async (values:never) => {
+    try {
+      setLoading(true);
+      const response = await loginUser(values);
+      alert("✅ Login successful!");
+      cookies.set("token", response.token);
+      navigate("/");
+    } catch (error) {
+      alert("❌ Login failed. Please check your credentials.");
+      console.error("Login failed:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -19,7 +33,7 @@ function LoginPage() {
         <Form
           className="flex flex-col gap-5 w-96"
           layout="vertical"
-         /*  onFinish={onFinish} */
+          onFinish={onFinish}
         >
           <h1 className="text-2xl font-bold text-gray-600">
             Login your account
--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/user-model");
 
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 
 // user registration
 
